af1b6533a6d8024b5fb0a35511988ed559de8449
melissaDerricottt <mderricott@higherachievement.org>
2018-12-10 21:23:23 -0500

    Find Route

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ var app = express();
 app.use(logger("dev"));
 
 //Parse request body as JSON
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 //Make public static folder
@@ -41,71 +43,74 @@ mongoose.connect(MONGODB_URI);
 
 
 // Get route for scrapping
-app.get("/api/washingtonpost/scrape", function(req, res){
+app.get("/api/washingtonpost/scrape", function (req, res) {
 
-    axios.get("https://www.washingtonpost.com/").then(function(response){
-    var $ = cheerio.load(response.data);
-    var results = [];
+    axios.get("https://www.washingtonpost.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+        var results = [];
 
-    $("div.pb-feature").each(function(i, element){
-        var text = $(element)
-            .children("div.border-top-off")
-            .children("div.flex-stack")
-            .children("div.headline")
-            .text()
-        var href = $(element)
-            .children("div.border-top-off")
-            .children("div.flex-stack")
-            .children("div.headline")
-            .children("a")
-            .attr("href")
-        var blurb = $(element)
-            .children("div.border-top-off")
-            .children("div.flex-stack")
-            .children("div.blurb")
-            .text()
-        var byline = $(element)
-            .children("div.border-top-off")
-            .children("div.flex-stack")
-            .children("ul.sigline")
-            .children("li.byline")
-            .text()
-        
-        // Artile information
-        results.push({
-            headline: text,
-            blurb: blurb,
-            byline: byline.trim(),
-            href: href
+        $("div.pb-feature").each(function (i, element) {
+            var text = $(element)
+                .children("div.border-top-off")
+                .children("div.flex-stack")
+                .children("div.headline")
+                .text()
+            var href = $(element)
+                .children("div.border-top-off")
+                .children("div.flex-stack")
+                .children("div.headline")
+                .children("a")
+                .attr("href")
+            var blurb = $(element)
+                .children("div.border-top-off")
+                .children("div.flex-stack")
+                .children("div.blurb")
+                .text()
+            var byline = $(element)
+                .children("div.border-top-off")
+                .children("div.flex-stack")
+                .children("ul.sigline")
+                .children("li.byline")
+                .text()
 
+            // Artile information
+            results.push({
+                headline: text,
+                blurb: blurb,
+                byline: byline.trim(),
+                href: href
+            });
+        })
+        res.json(results);
+    });
+
+});
+
+app.get("/api/articles", function (req,res){
+    db.Article.find({})
+        .then(function(dbArticle){
+            res.json(dbArticle)
+        })
+        .catch(function(err){
+            res.json(err);
         });
-    })
-    res.json(results);
-  
-
 });
 
-});
-
-
-app.post("/api/article/save", function(req, res){
-
+app.post("/api/article/save", function (req, res) {
     db.Article.create({
-        title: req.body.title,
-        articleUrl: req.body.articleUrl,
-        blurb: req.body.blurb,
-        byline: req.body.byline,     
-    })
-    .then(function(dbArticle){
-        console.log(dbArticle);
-        res.json(res)
-    })
-    .catch(function(err) {
-        // If an error occurred, log it
-        console.log(err);
-      });
-    
-    
+            title: req.body.title,
+            articleUrl: req.body.articleUrl,
+            blurb: req.body.blurb,
+            byline: req.body.byline,
+        })
+        .then(function (dbArticle) {
+            console.log(dbArticle);
+            res.json(res)
+        })
+        .catch(function (err) {
+            // If an error occurred, log it
+            console.log(err);
+        });
 });
 
 
@@ -123,6 +128,6 @@ app.post("/api/article/save", function(req, res){
 
 
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
 });
